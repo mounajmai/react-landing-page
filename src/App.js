20c55468a8f33c96a045e2b4d16e95ed8035645b
mouna jmai <mouna.jmai@gmail.com>
2024-09-13 11:32:56 +0100

    version 1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from './components/header';
+import Hero from './components/Hero';
+import Features from './components/Features';
+import Footer from './components/Footer';
+import './App.css'; // Import your styles
+function App() {
+return (
+<div className="App">
+<Header />
+<Hero />
+<Features />
+<Footer />
+</div>
+);
+}
+export default App;
